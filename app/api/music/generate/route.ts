e62e3e6bef9776920b60d9de24753fb14118c77e
Viesturs Marnauza <v.marnauza@gmail.com>
2023-09-04 18:29:37 +0300

    Implement fine-tuning

--- app/api/music/generate/route.ts
+++ app/api/music/generate/route.ts
@@ -1,8 +1,8 @@
 import { openai } from "@/backend/openai";
 import { GenerateRequestBody, Music } from "@/types/api";
 import { NextRequest, NextResponse } from "next/server";
-import { chords } from "@/backend/chords";
 import { Midi } from "@tonejs/midi";
+import { createPrompt, createSystemPrompt } from "@/backend/prompt";
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
@@ -25,23 +25,7 @@ export async function POST(request: NextRequest) {
     messages: [
       {
         role: "system",
-        content:
-          "You are a music composer. When asked by the user, try to create music that closely match their prompt. " +
-          "Step 1 - Generate a chord progression matching the user's request. For example, Dm7 - Fmaj7 - Em7 - Gmaj7" +
-          "Step 2 - Generate a melody that goes over the chords and matches the user's request. Melodies are typically higher in pitch than the underlying chord voices but can sometimes overlap with them. Rhythm and step-wise movement coupled with occasional leaps can make the melody satisfying and memorable. " +
-          "Respond with a JSON object containing one property called 'result'. " +
-          "The 'result' property must be an array of note events for all the chord and melody notes you generated. " +
-          "Each note event object contains properties for 'pitch', 'duration' and 'startTime'. " +
-          "The 'pitch' property is a string representing the note pitch and octave, for example, C3 or D#2. " +
-          "The 'duration' property is the note's duration in beats expressed as a float. " +
-          "The 'startTime' property is the note's start time in beats expressed as a float. " +
-          "Add all melody notes and chord voices as separate events. For example, for the chord DMaj7 add the notes D3 F#3 A3 and C#3. For Am add A3, C4 and E4. " +
-          // "The description property must be a string describing the music you generated. " +
-          "Adhere to the format outlined above exactly and do not add any additional text. Generate as many beats of music as requested by the user.\n",
-        // "Follow these steps to generate music:\n" +
-        // "Step 1 - Generate a melody matching the user's request. Add its notes to 'result' array.\n" +
-        // "Step 2 - Add matching chords to the melody. Add the notes of each of the voices in the chords to 'result' array.\n" +
-        // "Step 3 - Return the 'result' array and a description of the music you generated in a JSON object.",
+        content: createSystemPrompt(),
       },
       {
         role: "user",
@@ -79,27 +63,6 @@ export async function POST(request: NextRequest) {
     throw error;
   }
 }
-
-const createPrompt = ({
-  genre,
-  vibe,
-  style,
-  bars = 4,
-  customText,
-}: GenerateRequestBody) => {
-  let prompt = "";
-
-  if (vibe) prompt += vibe;
-  if (genre) prompt += ` ${genre} music`;
-  if (style) prompt += ` in style of ${style}`;
-  if (bars)
-    prompt += `. Create melody and chords for a total duration of ${
-      bars * 4
-    } beats of music.`;
-  if (customText) prompt += `, ${customText}`;
-
-  return prompt;
-};
 
 const isRequestBodyValid = (
   body: GenerateRequestBody
--- backend/fine-tune/index.ts
+++ backend/fine-tune/index.ts
@@ -1,38 +1,174 @@
 import dotenv from "dotenv";
-import { readFileSync, readdirSync } from "fs";
+import {
+  createReadStream,
+  existsSync,
+  readFileSync,
+  readdirSync,
+  statSync,
+  writeFileSync,
+} from "fs";
 import path from "path";
 import { Midi } from "@tonejs/midi";
+import { Melody } from "@/types/api";
+import { createSystemPrompt } from "../prompt";
+import { openai } from "../openai";
 
 dotenv.config({
   path: path.join(__dirname, "..", "..", ".env.local"),
 });
 
-const fineTune = (midis: Midi[]) => {
-  console.log(`Got ${midis.length} files for fine-tuning.`);
-  // console.log(midis[0].toJSON());
-  midis[0].toJSON().tracks.forEach((track) => {
-    console.log(`Track ${track.name}`);
-    track.notes.forEach((note) => {
-      console.log(note);
+type MidiData = Record<string, Midi[]>;
+interface Message {
+  role: "system" | "user" | "assistant";
+  content: string;
+}
+interface TrainingItem {
+  messages: Message[];
+}
+type TrainingData = TrainingItem[];
+
+const fineTune = async () => {
+  const trainingDataPath = path.join(__dirname, "training-data.jsonl");
+
+  if (existsSync(trainingDataPath)) {
+    let file = await openai.files.create({
+      file: createReadStream(trainingDataPath),
+      purpose: "fine-tune",
     });
-  });
+
+    console.log(`Uploaded file with ID: ${file.id}`);
+
+    console.log("-----");
+
+    console.log(`Waiting for file to be processed`);
+    while (true) {
+      file = await openai.files.retrieve(file.id);
+      console.log(`File status: ${file.status}`);
+
+      if (file.status === "processed") {
+        break;
+      } else {
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+      }
+    }
+
+    console.log("-----");
+
+    console.log(`Starting fine-tuning`);
+    let fineTune = await openai.fineTunes.create({
+      model: "gpt-3.5-turbo",
+      training_file: file.id,
+    });
+    console.log(`Fine-tuning ID: ${fineTune.id}`);
+
+    console.log("-----");
+
+    console.log(`Track fine-tuning progress:`);
+
+    const events: Record<string, unknown> = {};
+
+    while (fineTune.status == "running" || fineTune.status == "created") {
+      fineTune = await openai.fineTunes.retrieve(fineTune.id);
+      console.log(`${fineTune.status}`);
+
+      const { data } = await openai.fineTunes.listEvents(fineTune.id);
+      for (const event of data.reverse()) {
+        if (event.created_at in events) continue;
+
+        events[event.created_at] = event;
+        const timestamp = new Date(event.created_at * 1000);
+        console.log(`- ${timestamp.toLocaleTimeString()}: ${event.message}`);
+      }
+
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+    }
+  }
 };
 
 const readMidis = () => {
-  const fileList = readdirSync(path.join(__dirname, "midi"));
-  const midis: Midi[] = [];
+  const folderList = readdirSync(path.join(__dirname, "midi")).filter(
+    (folderName) => {
+      return statSync(path.join(__dirname, "midi", folderName)).isDirectory();
+    }
+  );
+  const midis: MidiData = {};
 
-  fileList.forEach((fileName) => {
-    console.log(`Reading ${fileName}`);
+  folderList.forEach((folderName) => {
+    console.log(`Reading folder ${folderName}`);
 
-    const file = readFileSync(path.join(__dirname, "midi", fileName));
-    const midi = new Midi(file);
+    const fileList = readdirSync(path.join(__dirname, "midi", folderName));
 
-    midis.push(midi);
+    midis[folderName] = [];
+    fileList.forEach((fileName) => {
+      console.log(`Reading file ${fileName}`);
+
+      const file = readFileSync(
+        path.join(__dirname, "midi", folderName, fileName)
+      );
+      const midi = new Midi(file);
+
+      midis[folderName].push(midi);
+    });
   });
 
   return midis;
 };
 
+const midiToGptResponse = (midi: Midi) => {
+  const response = {
+    music: {
+      result: [] as Melody,
+    },
+  };
+
+  midi.toJSON().tracks.forEach((track) => {
+    track.notes.forEach((note) => {
+      response.music.result.push({
+        pitch: note.name,
+        duration: note.duration,
+        startTime: note.time,
+      });
+    });
+  });
+
+  return response;
+};
+
+const midisToTrainingData = (midiData: MidiData) => {
+  const trainingData: TrainingData = [];
+
+  for (const [prompt, midis] of Object.entries(midiData)) {
+    midis.forEach((midi) => {
+      const response = midiToGptResponse(midi);
+      trainingData.push({
+        messages: [
+          {
+            role: "system",
+            content: createSystemPrompt(),
+          },
+          {
+            role: "user",
+            content: prompt,
+          },
+          {
+            role: "assistant",
+            content: JSON.stringify(response),
+          },
+        ],
+      });
+    });
+  }
+
+  writeFileSync(path.join(__dirname, "training-data.jsonl"), "");
+  trainingData.forEach((item) => {
+    writeFileSync(
+      path.join(__dirname, "training-data.jsonl"),
+      JSON.stringify(item) + "\n",
+      { flag: "a" }
+    );
+  });
+};
+
 const midis = readMidis();
-fineTune(midis);
+midisToTrainingData(midis);
+// fineTune();
